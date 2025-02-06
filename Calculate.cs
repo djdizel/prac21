7627f839a575d34d3aac3dcc6570a4137c23fd08
djdizel <158520404+djdizel@users.noreply.github.com>
2025-02-06 23:43:31 +0200

    num 1

---
+++ Calculate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac21
+{
+    internal class Calculate
+    {
+        private double number1;
+        private double number2;
+
+        public Calculate()
+        {
+            number1 = 0;
+            number2 = 0;
+        }
+
+        public Calculate(double value)
+        {
+            number1 = value;
+            number2 = value;
+        }
+
+        public Calculate(double value1, double value2)
+        {
+            number1 = value1;
+            number2 = value2;
+        }
+
+        public string GetInfo()
+        {
+            return $"Число 1: {number1}, Число 2: {number2}";
+        }
+
+        public double ProcessValues()
+        {
+            return Math.Sqrt(number1 * number2);
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -10,6 +10,25 @@ namespace prac21
     {
         static void Main(string[] args)
         {
+            Calculate obj1 = new Calculate();
+            Console.WriteLine(obj1.GetInfo());
+            Console.WriteLine("Результат: " + obj1.ProcessValues());
+
+            Calculate obj2 = new Calculate(5);
+            Console.WriteLine(obj2.GetInfo());
+            Console.WriteLine("Результат: " + obj2.ProcessValues());
+
+            Console.Write("Введите первое число: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Calculate obj3 = new Calculate(num1, num2);
+            Console.WriteLine(obj3.GetInfo());
+            Console.WriteLine("Результат: " + obj3.ProcessValues());
+
+            Console.ReadLine();
         }
     }
 }
